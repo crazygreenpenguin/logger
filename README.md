6d34ba6950304229495037c27b432c62ba8779b5
crazygreenpenguin <crazygreenpenguin@gmail.com>
2021-06-29 22:55:25 +0300

    Add simple performance test

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # logger
 Simple logger for go
+
+# Performance tests
+```
+goos: darwin
+goarch: amd64
+pkg: github.com/crazygreenpenguin/logger
+BenchmarkInfof-4      	  650191	      1597 ns/op	     432 B/op	      11 allocs/op
+BenchmarkInfo-4       	  736945	      1565 ns/op	     432 B/op	      11 allocs/op
+BenchmarkErrorf-4     	  726738	      1629 ns/op	     432 B/op	      11 allocs/op
+BenchmarkError-4      	  749658	      1589 ns/op	     432 B/op	      11 allocs/op
+BenchmarkWarningf-4   	  703910	      1643 ns/op	     432 B/op	      11 allocs/op
+BenchmarkWarning-4    	  753482	      1602 ns/op	     432 B/op	      11 allocs/op
+```
---
+++ logger_test.go
@@ -0,0 +1,66 @@
+package logger
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func BenchmarkInfof(b *testing.B) {
+	b.ReportAllocs()
+	b.StopTimer()
+	Init(ioutil.Discard,LStdFlags)
+	b.StartTimer()
+	for i:=0;i<b.N;i++ {
+		Infof("test message %d",1)
+	}
+}
+
+func BenchmarkInfo(b *testing.B) {
+	b.ReportAllocs()
+	b.StopTimer()
+	Init(ioutil.Discard,LStdFlags)
+	b.StartTimer()
+	for i:=0;i<b.N;i++ {
+		Info("test message 1")
+	}
+}
+
+func BenchmarkErrorf(b *testing.B) {
+	b.ReportAllocs()
+	b.StopTimer()
+	Init(ioutil.Discard,LStdFlags)
+	b.StartTimer()
+	for i:=0;i<b.N;i++ {
+		Errorf("test message %d",1)
+	}
+}
+
+func BenchmarkError(b *testing.B) {
+	b.ReportAllocs()
+	b.StopTimer()
+	Init(ioutil.Discard,LStdFlags)
+	b.StartTimer()
+	for i:=0;i<b.N;i++ {
+		Error("test message 1")
+	}
+}
+
+func BenchmarkWarningf(b *testing.B) {
+	b.ReportAllocs()
+	b.StopTimer()
+	Init(ioutil.Discard,LStdFlags)
+	b.StartTimer()
+	for i:=0;i<b.N;i++ {
+		Warningf("test message %d",1)
+	}
+}
+
+func BenchmarkWarning(b *testing.B) {
+	b.ReportAllocs()
+	b.StopTimer()
+	Init(ioutil.Discard,LStdFlags)
+	b.StartTimer()
+	for i:=0;i<b.N;i++ {
+		Warning("test message 1")
+	}
+}
